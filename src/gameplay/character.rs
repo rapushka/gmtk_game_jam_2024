534d4e0514eeda5bfc18d1c76bee0dfb5056c861
rapuska <mikalajsiatkouski@gmail.com>
2024-08-17 14:49:40 +0300

    spawn rounded square

--- src/gameplay/character.rs
+++ src/gameplay/character.rs
@@ -5,6 +5,8 @@ pub struct CharactersPlugin;
 impl Plugin for CharactersPlugin {
     fn build(&self, app: &mut App) {
         app
+            .register_type::<TextureSlicer>()
+
             .add_systems(OnEnter(AppState::in_gameplay()), spawn_character)
         ;
     }
@@ -14,8 +16,22 @@ fn spawn_character(
     mut commands: Commands,
     assets: Res<CommonAssets>,
 ) {
-    commands.spawn(SpriteBundle {
-        texture: assets.rounded_square.clone(),
-        ..Default::default()
-    });
+    let sprite_handle = assets.rounded_square.clone();
+
+    commands.spawn_with_name("background")
+        .insert(SpriteBundle {
+            texture: sprite_handle,
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(300.0, 100.0)),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(ImageScaleMode::Sliced(TextureSlicer {
+            border: BorderRect::square(15.0),
+            center_scale_mode: SliceScaleMode::Tile { stretch_value: 0.1 },
+            sides_scale_mode: SliceScaleMode::Tile { stretch_value: 0.2 },
+            max_corner_scale: 1.0,
+        }))
+    ;
 }
